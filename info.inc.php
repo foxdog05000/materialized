1176da9af93b1641e9d407ee028168a500cce8ed
Fox Orion <Fox Orion>
2016-08-06 00:19:39 +0200

    Start with existing theme : pmahomme

---
+++ info.inc.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Theme information
+ *
+ * @package    PhpMyAdmin-theme
+ * @subpackage materialized
+ */
+
+/**
+ * If you have problems or questions about this theme email
+ * foxdog.orion@gmail.com
+ */
+
+/**
+ * Special Thanks for design by Agustín (Source design : http://www.materialup.com/posts/phpmyadmin-material-design)
+ * Icon set used by this theme: https://design.google.com/icons/
+ */
+
+$theme_name = 'materialized';
+$theme_full_version = '0.1';
+?>
